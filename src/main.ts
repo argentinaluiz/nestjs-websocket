1edf3d0f6fdfa0de829024d387d96cd92775d555
Luiz Carlos <argentinaluiz@gmail.com>
2023-07-27 01:37:28 -0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './redis-io-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+
+  app.useWebSocketAdapter(redisIoAdapter);
   await app.listen(3000);
 }
 bootstrap();
